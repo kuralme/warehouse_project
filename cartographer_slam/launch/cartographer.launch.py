6541cb4b7de058da045451811f9a50b99779d8f7
kuralme <kuralme@protonmail.com>
2025-05-05 20:21:23 +0000

    added localization package - real warehouse map needs update - sim map updated

--- cartographer_slam/launch/cartographer.launch.py
+++ cartographer_slam/launch/cartographer.launch.py
@@ -1,39 +1,49 @@
 import os
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 
 def generate_launch_description():
 
+    use_sim_time = LaunchConfiguration('use_sim_time')
     cartographer_config_dir = os.path.join(get_package_share_directory('cartographer_slam'), 'config')
-    rviz_config_dir = os.path.join(get_package_share_directory('cartographer_slam'), 'rviz', 'mapper_rviz_config.rviz')
+    rviz_config_sim = os.path.join(get_package_share_directory('cartographer_slam'), 'rviz', 'mapping.rviz')
+    rviz_config_real = os.path.join(get_package_share_directory('cartographer_slam'), 'rviz', 'mapping_real.rviz')
     configuration_basename = 'cartographer_sim.lua'
+    configuration_basename_real = 'cartographer_real.lua'
 
     return LaunchDescription([
+
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use simulation (Gazebo) clock if true'),
         
         Node(
             package='cartographer_ros', 
             executable='cartographer_node', 
             name='cartographer_node',
             output='screen',
-            parameters=[{'use_sim_time': True}],
+            parameters=[{'use_sim_time': use_sim_time}],
             arguments=['-configuration_directory', cartographer_config_dir,
                        '-configuration_basename', configuration_basename]),
-
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            output='screen',
-            arguments=['-d', rviz_config_dir]
-        ),
 
         Node(
             package='cartographer_ros',
             executable='cartographer_occupancy_grid_node',
             output='screen',
             name='occupancy_grid_node',
-            parameters=[{'use_sim_time': True}],
+            parameters=[{'use_sim_time': use_sim_time}],
             arguments=['-resolution', '0.05', '-publish_period_sec', '1.0']
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_sim]
         ),
     ]) 
--- localization_server/launch/localization.launch.py
+++ localization_server/launch/localization.launch.py
@@ -1,18 +1,18 @@
 import os
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+
 from ament_index_python.packages import get_package_share_directory
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
+    
     map_file = LaunchConfiguration('map_file')
-
-    # map_yaml = os.path.join(get_package_share_directory('map_server'), 'config', 'warehouse_map_sim.yaml')
-    rviz_config_dir = os.path.join(get_package_share_directory('map_server'), 'rviz', 'map_display.rviz')
-    rviz_config_real = os.path.join(get_package_share_directory('map_server'), 'rviz', 'map_display_real.rviz')
+    nav2_yaml = os.path.join(get_package_share_directory('localization_server'), 'config', 'amcl_config_sim.yaml')
+    rviz_config_dir = os.path.join(get_package_share_directory('localization_server'), 'rviz', 'localizer_rviz_config.rviz')
 
     return LaunchDescription([
 
@@ -20,7 +20,16 @@ def generate_launch_description():
             'map_file',
             default_value='warehouse_map_sim.yaml',
             description='Name of the map YAML file inside map_server/config'),
-        
+
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='static_transform_publisher_turtle_odom',
+            output='screen',
+            emulate_tty=True,
+            arguments=['0', '0', '0', '0', '0', '0', 'map', 'robot_odom']
+        ),
+
         Node(
             package='nav2_map_server',
             executable='map_server',
@@ -32,15 +41,23 @@ def generate_launch_description():
                     FindPackageShare('map_server'),'config', map_file])
             }]
         ),
+        
+        Node(
+            package='nav2_amcl',
+            executable='amcl',
+            name='amcl',
+            output='screen',
+            parameters=[nav2_yaml]
+        ),
 
         Node(
             package='nav2_lifecycle_manager',
             executable='lifecycle_manager',
-            name='lifecycle_manager_mapper',
+            name='lifecycle_manager_localization',
             output='screen',
             parameters=[{'use_sim_time': True},
                         {'autostart': True},
-                        {'node_names': ['map_server']}]
+                        {'node_names': ['map_server', 'amcl']}]
         ),
 
         Node(
@@ -50,4 +67,4 @@ def generate_launch_description():
             output='screen',
             arguments=['-d', rviz_config_dir]
         )
-    ]) 
+    ])
